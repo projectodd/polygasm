d9ebd5a8d95bd5cbb6e37ef8afae263b50817b8f
Bob McWhirter <bob@mcwhirter.org>
2013-07-23 12:17:45 -0400

    Docs!

--- modules/fabric/src/main/java/org/projectodd/polygasm/fabric/ClusteringVerticle.java
+++ modules/fabric/src/main/java/org/projectodd/polygasm/fabric/ClusteringVerticle.java
@@ -7,7 +7,6 @@ import java.io.OutputStreamWriter;
 
 import org.jgroups.Address;
 import org.jgroups.Channel;
-import org.jgroups.Header;
 import org.jgroups.Message;
 import org.jgroups.Receiver;
 import org.jgroups.View;
@@ -17,23 +16,71 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.platform.Verticle;
 
+/**
+ * Useful base-class for coordinating clustered verticles.  
+ * 
+ * Within the {@link #start} method of the sub-class, a form of {@link #initChannel} should
+ * be called to initialize the underlying cluster-coordining JGroups channel.  A sub-classes
+ * {@link #stop} should call {@code super} to ensure the channel is correctly disconnected
+ * and stopped.
+ * 
+ * A sub-class my override {@link #startAsCoordinator()} and {@link #stopAsCoordinator()} if
+ * it desires to change behaviour based on if it is the coordinator (or master) instance within
+ * a cooperating cluster of instances.
+ * 
+ * Members of a clustered set may send JSON messages to all members using {@link #sendClusterMessage(JsonObject)}
+ * and may respond to messages send by overriding {@link #receiveClusterMessage(JsonObject)}.
+ * 
+ * State, as JSON objects, may be transfered between cluster members using the {@link #getState()} and {@link #setState(JsonObject)}
+ * methods.  By default, state is requested from the coordinating instance.
+ * 
+ * @author Bob McWhirter
+ */
 public class ClusteringVerticle extends Verticle {
 
     protected Channel channel;
     protected View view;
 
+    /** 
+     * Initialize and connect the underlying cluster channel.  The cluster name
+     * must be provided through the verticle's configuration using the {@code cluster}
+     * key.
+     * 
+     * @throws Exception
+     */
     protected void initChannel() throws Exception {
         initChannel(true);
     }
 
+    /** 
+     * Initialize and optionally connect the underlying cluster channel.  The cluster
+     * name must be provided through the verticle's configuration using the {@code cluster}
+     * key.
+     * 
+     * @param connect Determine if the channel should be automatically connected upon creation.
+     * @throws Exception
+     */
     protected void initChannel(boolean connect) throws Exception {
         initChannel(container.config().getString("cluster"), connect);
     }
 
+    /**
+     * Initialize and connect the underlying cluster channel.
+     * 
+     * @param cluster The cluster name.
+     * @throws Exception
+     */
     protected void initChannel(String cluster) throws Exception {
         initChannel(cluster, true);
     }
 
+    /**
+     * Initialize and optionally connecto the underlying cluster channel.
+     * 
+     * @param cluster The cluster name.
+     * @param connectDetermine if the channel should be automatically connected upon creation.
+     * @throws Exception
+     */
     protected void initChannel(String cluster, boolean connect) throws Exception {
         ChannelFactory factory = new ChannelFactory(this.container, this.vertx, cluster);
         this.channel = factory.newChannel();
@@ -54,14 +101,25 @@ public class ClusteringVerticle extends Verticle {
         }
     }
 
+    /**
+     * Retrieve the current state of the instance.
+     * 
+     * @return The state.
+     */
     protected JsonObject getState() {
         return null;
     }
 
+    /**
+     * Set the current state of the instance provided by 
+     * another instance.
+     * 
+     * @param state The state.
+     */
     protected void setState(JsonObject state) {
     }
 
-    protected void viewAccepted(View view) {
+    void viewAccepted(View view) {
         boolean priorCoordinator = isCoordinator();
         this.view = view;
         boolean currentCoordinator = isCoordinator();
@@ -73,6 +131,11 @@ public class ClusteringVerticle extends Verticle {
         }
     }
 
+    /**
+     * Determine if this instance is currently the coordinator (master).
+     * 
+     * @return {@code true} if currently the coordinator, otherwise {@code false}
+     */
     protected boolean isCoordinator() {
         if (this.view == null) {
             return false;
@@ -81,18 +144,41 @@ public class ClusteringVerticle extends Verticle {
         return this.view.getMembers().get(0).equals(this.channel.getAddress());
     }
 
+    /** 
+     * Called to inform the instance that it should start
+     * performing the role of coordinator (master) within
+     * a clustered set.
+     */
     protected void startAsCoordinator() {
 
     }
 
+    /**
+     * Called to inform the instance that it should stop
+     * performing the role of coordinator (master) within
+     * a clustered set.
+     */
     protected void stopAsCoordinator() {
 
     }
 
+    /**
+     * Send a message to all members of the clustered set
+     * (including self).
+     * 
+     * @param message The message to send.
+     * @throws Exception
+     */
     protected void sendClusterMessage(JsonObject message) throws Exception {
         this.channel.send(null, message.toString());
     }
 
+    /**
+     * Called to deliver a message to the instance from another
+     * member in the cluser (or self).
+     * 
+     * @param message The message received.
+     */
     protected void receiveClusterMessage(JsonObject message) {
 
     }
@@ -110,6 +196,11 @@ public class ClusteringVerticle extends Verticle {
         });
     }
 
+    /**
+     * Bridge class between JGroups channel and methods on the {@link ClusteringVerticle}.
+     * 
+     * @author Bob McWhirter
+     */
     class VerticleReceiver implements Receiver {
 
         private Context context;
